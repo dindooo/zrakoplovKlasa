5523385ed01db3674e3e54dba5d5a2f5cfd71031
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-29 09:31:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaZrakoplov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov x380 = new Zrakoplov("Airbus", "2000kW", "6000km");
+            Console.WriteLine(x380.ToString());
+
+            Zrakoplov C162 = new Zrakoplov("Cessna Skycatcher", "75kW", "870km");
+            Console.WriteLine(C162.ToString());
+
+            Zrakoplov PC21 = new Zrakoplov("Pilatus", "1200 kW", "1333 km");
+            Console.WriteLine(PC21.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zrakoplov.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaZrakoplov
+{
+    internal class Zrakoplov
+    {
+        string naziv;
+        string snagaMotora;
+        string dosegLeta;
+
+        public void setNaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+        public string getNaziv()
+        {
+            return this.naziv;
+        }
+        public void setSnagaMotora(string snagaMotora)
+        {
+            this.snagaMotora = snagaMotora ;
+        }
+        public string getSnagaMotora()
+        {
+            return this.snagaMotora;
+        }
+        public void setDosegLeta(string dosegLeta)
+        {
+            this.dosegLeta = dosegLeta;
+        }
+        public string getDosegLeta()
+        {
+            return this.dosegLeta;
+        }
+        public Zrakoplov(string naziv, string snagaMotora, string dosegLeta)
+        {
+            this.naziv = naziv;
+            this.snagaMotora = snagaMotora;
+            this.dosegLeta = dosegLeta;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Naziv " + this.getNaziv()
+            + " Snaga motora " + this.getSnagaMotora() 
+            + " Doseg leta " + this.getDosegLeta();
+            return ispis;
+        }
+    }
+}
